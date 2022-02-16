0ce7e80333f683cbe9cb89dc346a130ecd9f8074
julianapedroso <juliana.cristina@hotmail.com.br>
2022-02-16 12:56:50 -0300

    :tada: initial commit

---
+++ Jogo RPG/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Jogo_RPG.src.Entities;
+
+namespace Jogo_RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero arus = new Hero("Arus", 12, "Knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+
+            Console.WriteLine(arus.Attack());
+            // Reflete o polimorfismo, passando o int 3 como parâmetro
+            Console.WriteLine(wizard.Attack(3));
+        }
+    }
+}
