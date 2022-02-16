313055ff70fa1f64960be755d1775fb02179e768
julianapedroso <juliana.cristina@hotmail.com.br>
2022-02-16 12:57:57 -0300

    :sparkles: Hero class added

---
+++ Jogo RPG/src/Entities/Hero.cs
@@ -0,0 +1,27 @@
+namespace Jogo_RPG.src.Entities
+{
+    public class Hero
+    {
+        // Construct
+        public Hero(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+        public string Name;
+        public int Level;
+        public string HeroType;
+        
+        public override string ToString()
+        {
+            return this.Name + " " + this.Level + " " + this.HeroType;
+        }
+
+        public virtual string Attack()
+        {
+            return this.Name + ": atacou com a sua espada";
+        }
+    }
+}
