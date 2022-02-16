1f52fc3f0a6501da6e8a8f1e92d1f68941746ea0
julianapedroso <juliana.cristina@hotmail.com.br>
2022-02-16 13:01:28 -0300

    :sparkles: Wizard class added

--- Jogo RPG/src/Entities/Wizard.cs
+++ Jogo RPG/src/Entities/Wizard.cs
@@ -2,14 +2,15 @@ namespace Jogo_RPG.src.Entities
 {
     public class Wizard : Hero
     {
-        public Wizard(string Name, int Level, string HeroType) : base (Name, Level, HeroType)
+        public Wizard(string Name, int Level, string HeroType) :
+            base(Name, Level, HeroType)
         {
             this.Name = Name;
             this.Level = Level;
             this.HeroType = HeroType;
         }
 
-         public override string Attack()
+        public override string Attack()
         {
             return this.Name + ": lançou magia";
         }
